9ccc3bc7f554791b2a78958ba83cd6e300794082
Anthony Ng <thomsonng123@gmail.com>
2016-06-01 21:28:15 -0700

    added angular service

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 angular.module('flapperNews', [])
+.factory('posts', [function() {
+    var o = {
+        posts: [
+            {title: 'post 1', upvotes: 5},
+            {title: 'post 2', upvotes: 2},
+            {title: 'post 3', upvotes: 15},
+            {title: 'post 4', upvotes: 9},
+            {title: 'post 5', upvotes: 4}
+        ]
+    };
+    return o;
+}])
 .controller('MainCtrl', [
 '$scope',
-function($scope){
-    $scope.test = "Hello World!";
+'posts',
+function($scope, posts){
+    $scope.posts = posts.posts;
 
-    $scope.posts = [
-        {title: 'post 1', upvotes: 5},
-        {title: 'post 2', upvotes: 2},
-        {title: 'post 3', upvotes: 15},
-        {title: 'post 4', upvotes: 9},
-        {title: 'post 5', upvotes: 4},
-    ];
+    $scope.test = "Hello World!";
 
     $scope.addPost = function(){
         if(!$scope.title || $scope.title === '') {
