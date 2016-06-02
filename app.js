612b07ae4a44e3532b34f8886705faceb64ee3df
Anthony Ng <thomsonng123@gmail.com>
2016-06-01 17:28:42 -0700

    add ng-submit form for new post

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ angular.module('flapperNews', [])
 '$scope',
 function($scope){
     $scope.test = "Hello World!";
+
     $scope.posts = [
         {title: 'post 1', upvotes: 5},
         {title: 'post 2', upvotes: 2},
@@ -10,5 +11,10 @@ function($scope){
         {title: 'post 4', upvotes: 9},
         {title: 'post 5', upvotes: 4},
     ];
+
+    $scope.addPost = function(){
+        $scope.posts.push({title: $scope.title, upvotes: 0});
+        $scope.title = '';
+    };
 }]);
 
