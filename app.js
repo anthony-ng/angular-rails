d277ceaf71d3cc021e37dfb879552ad272a27907
Anthony Ng <thomsonng123@gmail.com>
2016-06-02 21:55:21 -0700

    added 2nd router

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ angular.module('flapperNews', ['ui.router'])
 '$stateProvider',
 '$urlRouterProvider',
 function($stateProvider, $urlRouterProvider) {
+
     $stateProvider.state('home', {
         url: '/home',
         templateUrl: '/home.html',
         controller: 'MainCtrl'
+    });
+
+    $stateProvider.state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
     });
 
     $urlRouterProvider.otherwise('home');
